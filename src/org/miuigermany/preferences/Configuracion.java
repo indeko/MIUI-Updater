1efe22c003c4c62bbdaa7ab43cac5295eb285c43
indeko <indeko@gmail.com>
2011-10-02 13:03:01 +0200

    first

--- src/org/miuigermany/preferences/Configuracion.java
+++
@@ -1,8 +0,0 @@
-package org.miuigermany.preferences;
-
-public class Configuracion {
-
-	public static final String direccionJSON = "http://test.miui-germany.de/cmtest/";
-	public static final String carpetaDescarga = "/sdcard/MIUI-Updater/";
-	
-}
